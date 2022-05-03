50a5681b6005fbc2d9d19397b68555f3f9301527
Will (Ubuntu) <gautreauxwill@gmail.com>
2022-05-03 04:39:12 -0400

    visualization of distance ga

---
+++ Distance_GA/main.py
@@ -0,0 +1,74 @@
+import numpy as np
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.optimize import minimize
+from pymoo.util.display import Display
+from MarioOperators import MarioMutationFurthest, MarioSampling, MarioProblemDistance, MarioTermination
+from pymoo.factory import get_crossover, get_visualization, get_termination
+import data
+import matplotlib.pyplot as plt
+
+
+# Intiialization variables
+data.pop_size = 5
+data.size = 1000
+data.furthest = 1
+data.time = 500
+data.ids = {}``
+data.times = [0] * data.pop_size
+
+
+term = get_termination("n_eval", 10)
+# with open('NumpyData.npy', 'rb') as f:
+#     print(np.load(f))
+
+
+# Total number of different GAs to use
+runs = 1
+i = 0
+distAlgorithm = GA(pop_size=data.pop_size, sampling=MarioSampling(), crossover=get_crossover('int_exp', prob=0.9), mutation=MarioMutationFurthest())
+bestPop = []
+
+
+# Finds the individual with the best distance fitness in the population
+def findMinIndiv(res):
+
+    minIndex = np.argmin(res.pop.get("F"))
+    finalArray = res.pop.get("X")
+    minIndiv = finalArray[minIndex]
+
+    return minIndiv
+
+
+# Runs different GAs "runs" amount of times then stores the values of the best indivs in each GA to NumpyData.npy
+while i < runs:
+
+    # Generates random int for seed
+    r = np.random.randint(10000000)
+    print("Seed: ", r)
+
+    # Resets data variables for each run
+    data.dist = 0
+    data.furthest = 1
+
+    # Runs minimization algorithm for GA
+    res = minimize(MarioProblemDistance(), distAlgorithm, seed=r, copy_algorithm=True, verbose=True, save_history=True, termination=MarioTermination())
+    
+    # Finds individual with best fitness for distance
+    minIndiv = findMinIndiv(res)
+
+    # Stores all the best individuals from each run into bestPop
+    bestPop.append(minIndiv)
+
+    i = i + 1
+
+n_evals = np.array([e.evaluator.n_eval for e in res.history])
+opt = np.array([e.opt[0].F for e in res.history])
+
+plt.title("Convergence")
+plt.plot(n_evals, 0 - opt, "--")
+plt.yscale("log")
+plt.show()
+
+# Saves bestPop into a file for later use
+# with open('NumpyData.npy', 'wb') as f:
+#     np.save(f, bestPop, allow_pickle=True)
